98a68026b7294758953e7206e708fa2bcb801916
Abhay <imabhaytiwari05@gmail.com>
2022-12-29 02:49:58 +0530

    home completed

--- index.js
+++ index.js
@@ -56,4 +56,18 @@ const timerFunction = setInterval(() => {
   }
 }, 1000);
 
-// console.log(countDownDate);
+
+// Event Slider JS
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
