2524fb35b4979e500c2ca3d866210d2ed9e1ec65
Abhay <imabhaytiwari05@gmail.com>
2022-12-28 13:33:01 +0530

    Adde Navbae and footer to all pages

--- team.js
+++ team.js
@@ -30,3 +30,24 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+  // Navbar JS
+
+$(".navTrigger").click(function () {
+	$(this).toggleClass("active");
+	console.log("Clicked menu");
+	$("#mainListDiv").toggleClass("show_list");
+	$("#mainListDiv").fadeIn();
+});
+
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+      $('.nav').addClass('affix');
+      $('.navTrigger').addClass('affix');
+      console.log("OK");
+  } else {
+      $('.nav').removeClass('affix');
+      $('.navTrigger').removeClass('affix');
+  }
+});
